54cf8d376089c509ccd62f83ea99da7079c22567
Abel S. Salvatierra <assalvatierra@gmail.com>
2021-03-25 07:34:14 +0800

    Update CarList.jsx

--- sample101/src/CarList.jsx
+++ sample101/src/CarList.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 
 class CarListClass extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+        this.getCarList()
+        .then(
+            res=>{
+                console.log(res);
+            }
+        ).catch(
+            err=>{
+                console.log(err);
+            }
+        )
+        ;
+    }
+
+    async getCarList(){
+        const sUrl = "https://127.0.0.11:49157/WeatherForecast";
+        const response = await fetch(sUrl);
+       // const _data = await response.json();
+        //console.log(_data);
+        return response.json();
+    }
 
     render(){
         const isready = true;
