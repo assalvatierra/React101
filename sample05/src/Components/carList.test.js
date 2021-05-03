dabce028c5a3f8a302574bc83cdfee437796775d
assalvatierra <assalvatierra@gmail.com>
2021-05-03 16:21:01 +0800

    update..

--- sample05/src/Components/carList.test.js
+++ sample05/src/Components/carList.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
+import CarList from './carList';
 
 let container = null;
 beforeEach( ()=> {
@@ -13,3 +14,25 @@ afterEach( ()=> {
     container.remove();
     container = null;
 })
+
+it('render car list', async() => {
+    const fakeList = 
+        [{id:0, name:'abel', remarks:'test'},
+        {id:1, name:'elvie', remarks:'test2'}];
+
+    jest.spyOn(global, 'fetch').mockImplementation(()=> 
+    Promise.resolve({
+        json: ()=>Promise.resolve(fakeList)
+    })   
+    );
+
+    await act( async()=>{
+        render(<CarList />, container);
+    });
+
+    expect(container.textContent).toContain('abel');
+    expect(container.textContent).toContain('elvie');
+
+    global.fetch.mockRestore();
+
+});
